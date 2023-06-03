d10cd6a75030394f2f376ae7363b07a799c52c68
julia-vi <86632925+julia-vi@users.noreply.github.com>
2023-06-03 12:54:52 -0300

    Initial commit

---
+++ BDLogistica/Qry_INSERT_INTO.sql
@@ -0,0 +1,10 @@
+USE Logistica;
+GO 
+INSERT INTO TableClientes (CodigoDoCliente, NomeDaEmpresa,NomeDoContato, CargoDoContato, Cidade, Pais)
+VALUES ( 'DEPLE', 'Deep Puple', 'Ian Gillan', 'Representante Comercial' , 'Osasco', 'Brasil') ;
+
+INSERT INTO TableClientes (CodigoDoCliente,  NomeDaEmpresa, NomeDoContato, CargoDoContato, Cidade, Pais)
+VALUES ('ACDC', 'ACDC', 'Angus McKinnon Young',  'Proprietario', 'São Paulo', 'Brasil');
+
+INSERT INTO TableClientes (CodigoDoCliente, NomeDaEmpresa, NomeDoContato, CargoDoContato, Cidade,  Pais)
+VALUES ('QUEENB', 'Queen', 'Freddie Mercury', 'Proprietario', 'Londres', 'Reino Unido');
